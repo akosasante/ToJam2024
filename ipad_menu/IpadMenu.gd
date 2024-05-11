be818aa5f7402e45e126d3179629bd91b19e9d96
Akosua Asante <aaasante@uwaterloo.ca>
2024-05-10 22:25:18 -0400

    Hover states on menu items

--- ipad_menu/IpadMenu.gd
+++ ipad_menu/IpadMenu.gd
@@ -8,34 +8,34 @@ static var menuItemScene: PackedScene = preload("res://ipad_menu/ipad_menu_item.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    var gridContainer: GridContainer = $Panel/GridContainer
-    print_debug("Got Grid: ", gridContainer)
+	var gridContainer: GridContainer = $Panel/GridContainer
+	print_debug("Got Grid: ", gridContainer)
 
-    # Initialize the shape of the grid
-    intializeNumberGridColumns(gridContainer, numAvailableDishes, numDishesPerRow)
+	# Initialize the shape of the grid
+	intializeNumberGridColumns(gridContainer, numAvailableDishes, numDishesPerRow)
 
-    # populate the menu with my test sushi images
-    for n in range(1, numAvailableDishes + 1):
-        var path: String = "res://akosua_test_images/sushi%s.png" % n
-        print_debug("path: ", path)
+	# populate the menu with my test sushi images
+	for n in range(1, numAvailableDishes + 1):
+		var path: String = "res://akosua_test_images/sushi%s.png" % n
+		print_debug("path: ", path)
 
-        var image: Texture2D = load(path)
-        instantiateDishMenuImage(gridContainer, image, "SUSHI #%s" % n)
+		var image: Texture2D = load(path)
+		instantiateDishMenuImage(gridContainer, image, "SUSHI #%s" % n)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-    pass
+	pass
 
 
 # initialize the grid container with a number of columns based on number of dishes
 # for now, let's say that we want to show max 3 dishes per row, so we'll divide numDishes by 3 to get numColumns
 func intializeNumberGridColumns(gridContainer: GridContainer, numDishes: int, perRow: int):
-    var numColumns: int = numDishes / perRow
-    gridContainer.columns = numColumns
+	var numColumns: int = numDishes / perRow
+	gridContainer.columns = numColumns
 
 
 func instantiateDishMenuImage(gridContainer: GridContainer, image: Texture2D, name: String):
-    var loadedMenuItem: IpadMenuItem = menuItemScene.instantiate().with_data(image)
-    loadedMenuItem.name = name
-    gridContainer.add_child(loadedMenuItem)
+	var loadedMenuItem: IpadMenuItem = menuItemScene.instantiate().with_data(image)
+	loadedMenuItem.name = name
+	gridContainer.add_child(loadedMenuItem)
