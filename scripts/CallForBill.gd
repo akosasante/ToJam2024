59addb48de095e184fed7eba6883e854736dcf7b
Akosua Asante <aaasante@uwaterloo.ca>
2024-05-11 17:58:21 -0400

    Call for bill button and script

---
+++ scripts/CallForBill.gd
@@ -0,0 +1,12 @@
+﻿extends Button
+
+@onready var button = $"."
+
+var end_game_scene := preload("res://scenes/end_screen.tscn")
+
+func _ready():
+	pass
+
+
+func _on_pressed():
+	SceneTransition.change_scene_with_dissolve(end_game_scene)
