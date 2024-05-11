6ff92f93f9287cb2cb2c2b9870a901791f1ec00f
richardJET <richardtillo@gmail.com>
2024-05-11 18:49:53 -0400

    added quit button

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -4,11 +4,14 @@ var audioMuted = false
 
 func _ready():
 	pass
-	
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_sound_button_pressed():
 	audioMuted = !audioMuted
 	var bus_idx = AudioServer.get_bus_index("Master")
 	AudioServer.set_bus_mute(bus_idx, audioMuted)
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_exit_button_pressed():
+	get_tree().quit()
