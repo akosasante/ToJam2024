4805e7cfac31d629d459c421851f4d8f19301904
Akosua Asante <aaasante@uwaterloo.ca>
2024-05-11 12:51:37 -0400

    some more reorg of ipad files

--- menu_globals.gd
+++ menu_globals.gd
@@ -4,10 +4,10 @@ extends Node
 # This starts at a number, let's say 9 for now
 # and changes depending on what's being ordered
 # later it will also be affected by the number of already ordered, but not-yet-eaten dishes on the table
-var remaining_capacity : int = 9
-
+var remaining_capacity: int = 9
+var foods_on_table := []
 signal remaining_capacity_changed
-	
+
 
 func remaining_capacity_change(value: int):
 	remaining_capacity += value
