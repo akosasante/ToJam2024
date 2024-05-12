61899d6bcfd9f19aace1f79c1847c92be0877b45
Akosua Asante <aaasante@uwaterloo.ca>
2024-05-11 23:25:25 -0400

    Add player HUD with connection to players eating

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,43 +1,48 @@
 extends Node2D
 
-const maxFull : int = 100
-var currentFull : int = 0
+const maxFull: int   = 100
+var currentFull: int = 0
 
-@onready var isFull : bool = false
+@onready var isFull: bool = false
+const maxIndigest: int   = 100
+var currentIndigest: int = 0
 
-const maxIndigest : int = 100
-var currentIndigest : int = 0
-@onready var hasIndigestion : bool = false
+@onready var hasIndigestion: bool = false
 
-@export var indigest_reduction_amount : int = 5
-@export var indigest_cooldown_wait_time : int = 10
+@export var indigest_reduction_amount: int = 5
+@export var indigest_cooldown_wait_time: int = 10
+
 @onready var indigest_cooldown_timer = $IndigestionCooldownTimer
+@onready var canEat: bool = true
 
-@onready var canEat : bool = true
+var water: FoodButton
 
-var water : FoodButton
+signal player_consumed_something
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	indigest_cooldown_timer.wait_time = indigest_cooldown_wait_time
-	
+
 	print ("Max Fullness: %s" % maxFull)
 	print ("Current Fullness: %s" % currentFull)
-	
+
 	print ("Max Indigestion: %s" % maxIndigest)
 	print ("Current Indigestion: %s" % currentIndigest)
-	
+
 	var res = []
 	MenuGlobals.findAllFoods(get_tree().current_scene, res)
 	for food in res:
 		if food.isWater:
 			water = food
 			break
-			
+
 	if (water):
 		water.food_eaten.connect(_on_kwasi_test_water_food_eaten)
 
 var timeVar = 0
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (!canEat && indigest_cooldown_timer.time_left > 0):
@@ -55,6 +60,7 @@ func _on_table_food_ready():
 		if !food.isWater:
 			food.food_eaten.connect(_on_kwasi_test_food_food_eaten)
 
+
 func _on_kwasi_test_food_food_eaten(food):
 	eat_drink_food(food, false)
 
@@ -62,7 +68,7 @@ func _on_kwasi_test_food_food_eaten(food):
 func _on_kwasi_test_water_food_eaten(food):
 	eat_drink_food(food, true)
 
-	
+
 func eat_drink_food(food, isWater):
 	if (food):
 		canEat = (isWater && !isFull) || (!isFull && !hasIndigestion)
@@ -70,12 +76,14 @@ func eat_drink_food(food, isWater):
 			currentFull += food.fullness
 			if (currentFull >= maxFull):
 				currentFull = maxFull
-			currentIndigest += food.indigestion
+			
+			currentIndigest += food.indigestion           
 			if (currentIndigest >= maxIndigest):
-					currentIndigest = maxIndigest
+				currentIndigest = maxIndigest
+			
 			print ("Current Fullness: %s" % currentFull)
 			print ("Current Indigestion: %s" % currentIndigest)
-			
+
 			if (currentFull >= maxFull):
 				currentFull = maxFull
 				isFull = true;
@@ -93,17 +101,21 @@ func eat_drink_food(food, isWater):
 					hasIndigestion = true
 					indigest_cooldown_timer.start()
 					print("You got indigestion. Wait until it goes down before eating")
+		
+			player_consumed_something.emit(food)
 		else:
 			print ("Current Fullness: %s" % currentFull)
 			print ("Current Indigestion: %s" % currentIndigest)
 			print("Player can't eat right now")
 	else:
 		print("FoodButton Is Null")
-		
+
+
 func time_left_until_eat():
 	var time_left = indigest_cooldown_timer.time_left
-	var second = int(time_left) % 60
+	var second    = int(time_left) % 60
 	return second
+
 
 func _on_indigestion_cooldown_timer_timeout():
 	currentIndigest -= indigest_reduction_amount
